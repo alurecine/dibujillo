3c2d9ae98e30f2a03ec64997413a320e073c0e5b
Alan Recine <alurecine@gmail.com>
2026-02-28 10:26:52 -0300

    Initial Commit

---
+++ Dibujillo/DibujilloApp.swift
@@ -0,0 +1,17 @@
+//
+//  DibujilloApp.swift
+//  Dibujillo
+//
+//  Created by Alan Recine on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct DibujilloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
